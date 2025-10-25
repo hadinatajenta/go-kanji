15a11b5b68ceb25f9ebc412942c0acc1172d3b32
Hadinata Jenta <70001259>
2025-10-25 18:36:08 +0700

    connect to db psql

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"gobackend/infra/db"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := db.OpenConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Successfully connected to the database!")
+}
